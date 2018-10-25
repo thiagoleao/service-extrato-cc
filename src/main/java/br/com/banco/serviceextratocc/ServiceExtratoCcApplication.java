851ed1656faa6e3d289131ef608acd806109f084
Thiago Leão <thiago.6286@gmail.com>
2018-10-25 07:01:08 -0300

    Criação da estrutura do projeto

---
+++ src/main/java/br/com/banco/serviceextratocc/ServiceExtratoCcApplication.java
@@ -0,0 +1,12 @@
+package br.com.banco.serviceextratocc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExtratoCcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceExtratoCcApplication.class, args);
+	}
+}
