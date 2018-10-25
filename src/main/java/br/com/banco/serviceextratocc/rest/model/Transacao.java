d8a322becb5d5398185eed809dc27fe959a47070
Thiago Leão <thiago.6286@gmail.com>
2018-10-25 09:27:45 -0300

    Exemplo de teste de controller

--- src/main/java/br/com/banco/serviceextratocc/rest/model/Transacao.java
+++ src/main/java/br/com/banco/serviceextratocc/rest/model/Transacao.java
@@ -3,18 +3,16 @@ package br.com.banco.serviceextratocc.rest.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transacao implements Serializable {
 	
 	private static final long serialVersionUID = -8955392979477844705L;
-	
-	public Transacao(String data, BigDecimal valor, String tipoTransacao) {
-		this.data = data;
-		this.valor = valor;
-		this.tipoTransacao = tipoTransacao;
-	}
 	
 	private String data;
 	private BigDecimal valor;
